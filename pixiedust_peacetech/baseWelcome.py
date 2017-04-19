e87b61c2cf14ecb5e1ba79a42e39d3cbdc20857e
David Taieb <david_taieb@us.ibm.com>
2017-04-19 15:29:01 -0400

    Training data generation app

--- pixiedust_peacetech/baseWelcome.py
+++ pixiedust_peacetech/baseWelcome.py
@@ -1,9 +1,35 @@
 from pixiedust.display.app import *
-from pixiedust.utils import Logger
+from pixiedust.utils import Logger, cache
+from pixiedust.utils.shellAccess import ShellAccess
+import requests
+from pandas.io.json import json_normalize
+import pandas
+import json
+
+baseUrl = "https://j26ovra6z7.execute-api.us-east-1.amazonaws.com/prod/"
+indicators = ["Economy", "Energy", "Law & Order", "Environment", "Labor"]
 
 @PixieApp
 @Logger()
 class BaseWelcome():
+    indicators = ["Economy", "Energy", "Law & Order", "Environment", "Labor"]
+    
+    @cache(fieldName="_alerts")
+    def getAlerts(self, startDate, endDate=None, location=None):
+        if location is not None and endDate is not None:
+            restUrl = "{0}alerts/search?startDate={1}&endDate={2}&location={3}".format(baseUrl, startDate, endDate, location)
+            return self.normalize(requests.get(restUrl, headers=ShellAccess.headers).json())
+        else:
+            return self.normalize(requests.get(baseUrl+"alerts?eventDate="+eventDate, headers=headers).json())
+
+    def normalize(self, jsonPayload):
+        if len(jsonPayload) > 0:
+            return json_normalize(jsonPayload)
+        return pandas.DataFrame()
+    
+    def getAlert(self, alertKey):
+        resp = requests.get(baseUrl+"alerts/"+alertKey, headers=ShellAccess.headers).json()
+        return self.normalize(resp)
     
     #subclass can override
     def onCountrySelected(self):
@@ -11,6 +37,8 @@ class BaseWelcome():
     
     @route()
     def baseWelcome(self):
+        if ShellAccess.headers is None:
+            return "<div>Error, you must define the GroudTruth DataHub credentials in a variable called headers</div>"
         return """
 <div>
   <div class="form-horizontal">
